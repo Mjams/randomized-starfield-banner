2646b6cae35b643d8521be4827e844906f5450c5
Mjams <maggie.paquette@hotmail.ca>
2016-04-01 10:54:50 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 var $body = $('.banner');
 var $star;
+var bannerWidth = $body.outerWidth();
+var bannerHeight = $body.outerHeight();
 
 for (i = 0; i < 75; i++) {
   $star = $('<div>');
   $star.addClass('star');
   $star.css({
-    'top': Math.random() * (document.documentElement.clientHeight + 100),
-    'left': Math.random() * (document.documentElement.clientHeight + 100),
-    'opacity': Math.random(),
-    'transform': Math.random()
+    top: Math.random() * bannerHeight,
+    left: Math.random() * bannerWidth,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
   });
   $body.append($star);
 }
